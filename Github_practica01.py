0d543d778c6ce888d724f1f39edc8220d34d2a5f
pere.miralles <pere.miralles@escolaindustrial.org>
2022-11-18 13:01:42 +0100

    PereM Numero 23

--- Github_practica01.py
+++ Github_practica01.py
@@ -159,7 +159,7 @@ def tronc_piramide(): # 22
     
     return area, volum
  
-def casquet_esferic(): # 23
+def casquet_esferic(): # 23 Pere M.
     print("Càlcul de l'àrea i del volum d'un casquet_esfèric ")
     r = float(input("Radi = "))
     h = float(input("Alçada del casquet = "))
