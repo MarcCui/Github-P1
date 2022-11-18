fd1671aa9eb12be812e875869776a61eeba28d70
eduardo.otoni <eduardo.otoni@escolaindustrial.org>
2022-11-18 13:03:08 +0100

    Eduardo O numero 25

--- Github_practica01.py
+++ Github_practica01.py
@@ -170,7 +170,16 @@ def casquet_esferic(): # 23
 def fus_falcaEsferica(): # 24
     
     
-def segment_esferic(): # 25
+def segment_esferic(): # 25 Eduardo Otoni
+    print("Càlcul de l'àrea i del volum d'un segment esfèric ")
+    h = float(input("Alçada de la zona o segment esfèric = "))
+    R = float(input("Radi de l'esfera = "))
+    r_gran = float(input("Radi gran del segment = "))
+    r_petit = float(input("Radi petit del segment = "))
+    area = 2 * PI * R * h
+    volum = 1/6 * PI * h * (pow(h,2) + 3 * pow(r_gran,2) + 3 * pow(r_petit,2))
+    return area, volum
+    
 
 # Els alumnes 27 i 28 buscaran les taules a treballar i comprobaran resultats d'execució
 # Programa principal  Alumn@ 27
